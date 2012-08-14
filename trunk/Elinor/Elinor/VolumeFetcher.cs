f4c20239c46b378125868b75cb2dda6f8c3c521f
stengah.dn@gmail.com <stengah.dn@gmail.com@ab8fbe74-e290-f9ad-cbea-a2286898d62b>
2012-08-14 17:24:55 +0000

    removed webscraping from eve-central

--- trunk/Elinor/Elinor/VolumeFetcher.cs
+++ trunk/Elinor/Elinor/VolumeFetcher.cs
@@ -1,73 +1,49 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
-using System.Net;
-using System.Text;
-using System.Text.RegularExpressions;
 
 namespace Elinor
 {
     class VolumeFetcher
     {
-        internal static Dictionary<string, long> GetVolumes(int typeId, int systemId)
+        internal static Dictionary<string, long> GetVolumes(List<List<string>> table)
         {
+            long sellvol = 0, buyvol = 0;
+            long sellinit = 0, buyinit = 0;
             var result = new Dictionary<string, long>();
+            
+            var inrange = from List<string> row in table
+                       where row[13] == "0"
+                       select row;
 
-            string html = GetHtml(typeId, systemId);
-            if(html != null)
+            foreach(var v in inrange)
             {
-                /*  <b>Volume for sale now:</b> 46 units
-                    <br />
-                    <b class="view">Movement <span>(Initial volume - volume remaining)</span></b>: 0 units */   
-                var dunits = new Regex(@"[\d,]+ units");
-                var matches = dunits.Matches(html);
-
-                int step = 0;
-                foreach (Match m in matches)
+                long l;
+                double d;
+                if(v[7] == "False")
                 {
-                    long i;
-                    if(long.TryParse(m.Value.Replace("units", "").Trim(), NumberStyles.Any, CultureInfo.InvariantCulture, out i))
-                    {
-                        switch (step)
-                        {
-                            case 0:
-                                result.Add("sellvol", i);
-                                break;
-                            case 1:
-                                result.Add("sellmov", i);
-                                break;
-                            case 2:
-                                result.Add("buyvol", i);
-                                break;
-                            case 3:
-                                result.Add("buymov", i);
-                                break;
-                        }
-                    }
-                    step++;
+                    if(double.TryParse(v[1], NumberStyles.Any, CultureInfo.InvariantCulture, out d))
+                        sellvol += (long)d;
+                    if (long.TryParse(v[5], NumberStyles.Any, CultureInfo.InvariantCulture, out l))
+                        sellinit += l;
                 }
-
-
-
-            }
-            return result;
-        }
-
-        private static string GetHtml(int typeId, int systemId)
-        {
-            string result;
-            using (var client = new WebClient())
-            {
-                try
+                else if(v[7] == "True")
                 {
-                    result = client.DownloadString(string.Format("http://eve-central.com/home/quicklook.html?typeid={0}&usesystem={1}", typeId, systemId));
-                }
-                catch (Exception)
-                {
-                    result = null;
+                    if (double.TryParse(v[1], NumberStyles.Any, CultureInfo.InvariantCulture, out d))
+                        buyvol += (long)d;
+                    if (long.TryParse(v[5], NumberStyles.Any, CultureInfo.InvariantCulture, out l))
+                        buyinit += l;
                 }
             }
+
+            long selldiff = sellinit - sellvol;
+            long buydiff = buyinit - buyvol;
+
+            result.Add("sellvol", sellvol);
+            result.Add("sellmov", selldiff);
+            result.Add("buyvol", buyvol);
+            result.Add("buymov", buydiff);
+
             return result;
         }
     }
